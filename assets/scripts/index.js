73e0a075198c5a7614ab9e7242b4dc6386c4191e
Kevin González <44679178+kevinglezgmz@users.noreply.github.com>
2021-09-28 11:09:21 -0500

    feat: built request functionality to use with future search input and button (#3)

---
+++ assets/scripts/index.js
@@ -0,0 +1,25 @@
+import { PRIVATE_KEY } from "../../assets/scripts/privateKey.js";
+const searchBtn = document.getElementById("search-btn");
+function fetchNews(event) {
+    const newsUri = getNewsUri();
+    axios.get().then(setNewsHandlebars).catch(logRequestError);
+}
+function getNewsUri() {
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    const from = yesterday.toDateString();
+    const searchInput = document.getElementById("search-input");
+    const q = searchInput.value;
+    return `https://newsapi.org/v2/everything?q=${q}&from=${from}&sortBy=popularity&apiKey=${PRIVATE_KEY}`;
+}
+function setNewsHandlebars(response) {
+    const templateSource = document.getElementById("grid-source").innerHTML;
+    const template = Handlebars.compile(templateSource);
+    document.getElementById("grid").innerHTML = template({
+        news: response.data.articles,
+    });
+}
+function logRequestError(err) {
+    console.log(err);
+}
+searchBtn.addEventListener("click", fetchNews);
---
+++ src/scripts/index.ts
@@ -0,0 +1,36 @@
+import { PRIVATE_KEY } from "../../assets/scripts/privateKey.js";
+
+declare let Handlebars;
+declare let axios;
+
+const searchBtn = <HTMLButtonElement>document.getElementById("search-btn");
+
+function fetchNews(event: MouseEvent) {
+  const newsUri = getNewsUri();
+  axios.get().then(setNewsHandlebars).catch(logRequestError);
+}
+
+function getNewsUri(): string {
+  const yesterday = new Date();
+  yesterday.setDate(yesterday.getDate() - 1);
+  const from = yesterday.toDateString();
+
+  const searchInput = <HTMLInputElement>document.getElementById("search-input");
+  const q = searchInput.value;
+
+  return `https://newsapi.org/v2/everything?q=${q}&from=${from}&sortBy=popularity&apiKey=${PRIVATE_KEY}`;
+}
+
+function setNewsHandlebars(response: any) {
+  const templateSource = document.getElementById("grid-source").innerHTML;
+  const template = Handlebars.compile(templateSource);
+  document.getElementById("grid").innerHTML = template({
+    news: response.data.articles,
+  });
+}
+
+function logRequestError(err: any) {
+  console.log(err);
+}
+
+searchBtn.addEventListener("click", fetchNews);
