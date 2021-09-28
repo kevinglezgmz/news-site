34f2fa1225a40d2ae19fd028b200e3204a93ed68
Kevin Gonzalez <kevingonzalezgmz@gmail.com>
2021-09-28 11:08:10 -0500

    feat: built request functionality to use with future search input and button

---
+++ src/scripts/index.ts
@@ -0,0 +1,16 @@
+declare let Handlebars;
+declare let axios;
+
+import { PRIVATE_KEY } from "../../assets/scripts/privateKey.js";
+axios
+  .get(`https://newsapi.org/v2/everything?q=Apple&from=2021-09-28&sortBy=popularity&apiKey=${PRIVATE_KEY}`)
+  .then((response) => {
+    const templateSource = document.getElementById("grid-source").innerHTML;
+    const template = Handlebars.compile(templateSource);
+    document.getElementById("grid").innerHTML = template({
+      news: response.data.articles,
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
